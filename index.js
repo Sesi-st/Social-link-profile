3816613c392ccf79d64f5526e789e127a37125b0
Sesi-st <Sesinamattipoe@icloud.com>
2024-03-07 01:10:12 +0000

    did the phone view to fill the whole screen

---
+++ index.js
@@ -0,0 +1,25 @@
+let gitHub  = document.getElementById("gitHubBtn");
+let instagram = document.getElementById("instagramBtn");
+let twitter = document.getElementById("twitterBtn");
+let linkedin = document.getElementById("linkedinBtn");
+let email = document.getElementById("email");
+
+gitHub.onclick = function(){
+    window.open('https://github.com/Sesi-st')
+}
+
+instagram.onclick = function(){
+    window.open('https://WWW.instagram.com/sesinam_._/');
+}
+
+twitter.onclick = function(){
+    window.open('https://twitter.com/attipoe_stephen');
+}
+
+linkedin.onclick = function(){
+    window.open('https://WWW.linkedin.com/in/sesi-inthecut-a01aaa2b8');
+}
+
+email.onclick = function(){
+    window.open('mailto:sesinamattipoe360@gmail.com');
+}
